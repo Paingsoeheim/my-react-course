d8ff2db5de1b5b5234a173f88eb20e215c036fd5
Paingsoeheim <paingsoehein3579@gmail.com>
2024-04-27 18:06:09 +0630

    second commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const name = "paing";
+const age = 22;
+const arr = [1, 2, 3, 4];
+let element;
+function human() {
+  for (let i = 0; i < arr.length; i++) {
+    element = arr[i];
+  }
+}
+human();
